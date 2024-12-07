d661f19c1c1126e387aabc6fd7bc348c243ff201
Ana Maria Ortiz <amor170294@gmail.com>
2024-12-07 17:24:59 +0000

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,8 +1,7 @@
-//JanaCraftShop web design Jessica Kelechi Ezema and Ana Maria Ortiz Ramirez
-
+// JanaCraftShop web design Jessica Kelechi Ezema and Ana Maria Ortiz Ramirez
+// Code to display an automatic slideshow. Code from w3schools
 let slideIndex = 0;
 showSlides();
-
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
@@ -12,5 +11,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
